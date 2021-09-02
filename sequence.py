9616d3d7d4d6910352a0ab8bbd7eb656c7455a95
Gnaeus-Pompeius <smarib20@ru.is>
2021-09-02 10:43:14 +0000

    plis virka

--- sequence.py
+++ sequence.py
@@ -1,1 +1,6 @@
-#####Algorithm here
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+if
+
+print("The maximum is", max_int)    # Do not change this line
